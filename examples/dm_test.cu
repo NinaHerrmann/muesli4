db91e08f02aea551bc7681b800522668feabaa35
Nina Herrmann <nina.herrmann@wi.uni-muenster.de>
2020-11-04 16:02:47 +0100

    added ReadmeNH

--- examples/dm_test.cu
+++ examples/dm_test.cu
@@ -36,54 +36,54 @@
 #include "dm.h"
 
 namespace msl {
-namespace test {
+    namespace test {
 
-class Square : public Functor<int, int> {
-public: MSL_USERFUNC int operator() (int x) const {return x*x;}
-};
+        class Square : public Functor<int, int> {
+        public: MSL_USERFUNC int operator() (int x) const {return x*x;}
+        };
 
-class Mult : public Functor<int, int> {
-private: int y;
-public:
-    Mult(int factor):
-        y(factor){}
+        class Mult : public Functor<int, int> {
+        private: int y;
+        public:
+            Mult(int factor):
+                y(factor){}
 
-    MSL_USERFUNC int operator() (int x) const {return x*y;}
-};
+            MSL_USERFUNC int operator() (int x) const {return x*y;}
+        };
 
-struct consti : public Functor2<int, int, int>{ // public msl::AMapIndexFunctor<int, int>{
-    MSL_USERFUNC int operator()(int i, int Ai) const {return i;}
-};
+        struct consti : public Functor2<int, int, int>{ // public msl::AMapIndexFunctor<int, int>{
+            MSL_USERFUNC int operator()(int i, int Ai) const {return i;}
+        };
 
-class Sum : public Functor2<int, int, int>{
-public: MSL_USERFUNC int operator() (int x, int y) const {return x+y;}
-};
+        class Sum : public Functor2<int, int, int>{
+        public: MSL_USERFUNC int operator() (int x, int y) const {return x+y;}
+        };
 
-void dm_test(int dim) {
-  //printf("Starting dm_test...\n");
-  DM<int> a(10,10, 2);
-  //a.show("a1");
+        void dm_test(int dim) {
+          //printf("Starting dm_test...\n");
+          DM<int> a(10,10, 2);
+          //a.show("a1");
 
-  Square sq;
-  a.mapInPlace(sq);
-  //a.show("a2");
+          Square sq;
+          a.mapInPlace(sq);
+          a.show("a2");
 
-  Mult mult(3);
-  a.mapInPlace(mult);
-  //a.show("a3");
+          Mult mult(3);
+          a.mapInPlace(mult);
+          a.show("a3");
 
-  consti pr;
-  a.mapIndexInPlace(pr);
-  a.show("a4");
+          consti pr;
+          a.mapIndexInPlace(pr);
+          a.show("a4");
 
-  Sum sum;
-  int result = a.fold(sum,true);
-  printf("result: %i\n",result);
-  a.show("a5");
+          Sum sum;
+          int result = a.fold(sum,true);
+          printf("result: %i\n",result);
+          a.show("a5");
 
-  return;
-}
-}} // close namespaces
+          return;
+        }
+    }} // close namespaces
 
 int main(int argc, char** argv){
   //printf("Starting Main...\n");
@@ -94,4 +94,3 @@ int main(int argc, char** argv){
   msl::terminateSkeletons();
   return EXIT_SUCCESS;
 }
-
